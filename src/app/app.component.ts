a9e0fdcc0c257c5a5e3cdbead849bf29381ae9f2
rickaed <wcsrickaed22@gmail.com>
2023-01-27 21:52:49 +0100

    add header & list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
+
 
 @Component({
   selector: 'app-root',
@@ -8,58 +8,12 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit {
 
- faceSnaps!:FaceSnap[];
-
-
-  ngOnInit() {
-    this.faceSnaps = [
-      {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis tout petit !',
-        createdDate: new Date(),
-        snaps: 1,
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        location: "Paris"
-      },
-      {
-        title: 'Three Rock Mountain',
-        description: 'Un endroit magnifique pour les randonnées.',
-        createdDate: new Date(),
-        snaps: 2,
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-        location: "La montagne"
-      },
-      {
-        title: 'Un bon repas',
-        description: 'Mmmh que c\'est bon !',
-        createdDate: new Date(),
-        snaps: 3,
-        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg'
-      },
-      {
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis tout petit !',
-        createdDate: new Date(),
-        snaps: 4,
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        location: "Paris"
-      },
-      {
-        title: 'Three Rock Mountain',
-        description: 'Un endroit magnifique pour les randonnées.',
-        createdDate: new Date(),
-        snaps: 5,
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-        location: "La montagne"
-      },
-      {
-        title: 'Un bon repas',
-        description: 'Mmmh que c\'est bon !',
-        createdDate: new Date(),
-        snaps: 6,
-        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg'
-      }
-    ];
+  ngOnInit(): void {
 
   }
 }
+
+
+
+
+
